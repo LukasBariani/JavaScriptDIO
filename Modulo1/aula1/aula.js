157d8d36209d5fcd632be7aa9a7419f213eb0e41
Lukas Bariani <lukasbariani08@gmail.com>
2025-06-18 09:23:24 -0300

    organizando

---
+++ Modulo1/aula1/aula.js
@@ -0,0 +1,7 @@
+const precoCombustivel = 5.79
+const consumo = 10
+let distancia = 100
+
+total = (distancia / consumo) * precoCombustivel
+
+console.log(total.toFixed(100));
--- aula1/aula.js
+++
@@ -1,7 +0,0 @@
-const precoCombustivel = 5.79
-const consumo = 10
-let distancia = 100
-
-total = (distancia / consumo) * precoCombustivel
-
-console.log(total.toFixed(100));
